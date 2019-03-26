2b43c32aad6c994be018ef679384882e76873cb4
IamHereWaitingForYou <44939063+IamHereWaitingForYou@users.noreply.github.com>
2019-03-26 17:11:12 +0800

    Add files via upload

---
+++ 分析输入定稿.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	double *p = new double[n];
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p[i];
+		sum += p[i];
+	}
+	double a = sum / n;
+	cout << a << endl;
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (p[i] > a)
+			count++;
+	}
+	cout << count << endl;
+	return 0;
+}
+
